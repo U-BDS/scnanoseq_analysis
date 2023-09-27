68b6636559e83368d0cba05d0aeafbcad125ab09
Lara Ianov <lianov@uab.edu>
2023-09-27 16:21:19 -0500

    moved cust. functions to R scripts. Made a few changes as well, including workaround to tryCatch

---
+++ tertiary_analysis/scripts/R/plot_markers_by_list.R
@@ -0,0 +1,65 @@
+#' plot_markers_by_list
+#' Plots a series of figures for marker visualization: FeaturePlot, violin plots and dot plot
+#' @param seurat_obj seurat object
+#' @param marker_lst vector of features to plot
+#' @param out_dir_lst output directory
+#' @param out_prefix_lst output prefix
+#'
+#' @return
+#' @export
+
+plot_markers_by_list <- function(seurat_obj, 
+                                 marker_lst,
+                                 out_dir_lst = './marker_plots/',
+                                 out_prefix_lst = ''){
+  # Given a list of markers, this will generate a feature plot, violin plot,
+  # and a dot plot for the seurat object that is passed in
+  dir.create(file.path(out_dir_lst), recursive = TRUE, showWarnings = FALSE)
+  
+  # Feature Plot
+  png(paste0(out_dir_lst, out_prefix_lst, "markers_featureplot.png"),
+      width = 1024,
+      height = 768)
+  
+  plot(
+    FeaturePlot(object = seurat_obj,
+                features = marker_lst,
+                reduction = "umap")
+  )
+  
+  dev.off()
+  
+  # Violin Plot
+  png(paste0(out_dir_lst, out_prefix_lst, "markers_violinplot.png"),
+      width = 1024,
+      height = 768)
+  
+  plot(
+    VlnPlot(object = seurat_obj,
+            split.by = "seurat_clusters",
+            features = marker_lst,
+            assay = "RNA")
+  )
+  
+  dev.off()
+  
+  # Dot Plot
+  png(paste0(out_dir_lst, out_prefix_lst, "markers_dotplot.png"),
+      width = 1024,
+      height = 768)
+  
+  plot(
+    DotPlot(object = seurat_obj,
+            features = marker_lst,
+            group.by = "seurat_clusters",
+            assay = "RNA",
+            col.min= -1.5,
+            col.max= 1.5,
+            scale= TRUE,
+            cluster.idents=FALSE,
+            cols = "RdYlBu")  + coord_flip()
+  )
+  
+  dev.off()
+  
+}
---
+++ tertiary_analysis/scripts/R/plot_markers_by_mtx.R
@@ -0,0 +1,62 @@
+#' plot_markers_by_mtx
+#' For gene or transcript feature, this functions calls plot_markers_by_list
+#' @param seurat_obj seurat object
+#' @param marker_mtx data.frame of gene and transcript markers. First column must be named "gene", and second "transcript"
+#' @param feature feature type: `gene` or `transcript`
+#' @param out_dir_mtx output directory
+#' @param out_prefix_mtx output prefix
+#'
+#' @return
+#' @export
+
+plot_markers_by_mtx <- function(seurat_obj,
+                                marker_mtx,
+                                feature,
+                                out_dir_mtx = './marker_plots/', 
+                                out_prefix_mtx = ''){
+  # This will grab the correct columns from the matrix and try to plot the 
+  # markers for the seurat object. This will also create help split up markers
+  # in the case for the transcript so they are easily navigable.
+  if (feature == 'gene'){
+    
+    # filter marker to only IDs present in obj:
+    status <- unique(marker_mtx$gene) %in% rownames(seurat_obj@assays$RNA@counts)
+    markers_filtered <- unique(marker_mtx$gene)[status]
+    
+    if (length(markers_filtered > 0)) {
+      
+      plot_markers_by_list(seurat_obj,
+                           markers_filtered,
+                           paste0(out_dir_mtx, feature, '/'),
+                           out_prefix_mtx)
+      
+    } else {
+      
+      message("No genes from input markers were found in the current object")
+      
+    }
+    
+  } else if (feature == 'transcript'){
+    
+     for (g in unique(marker_mtx$gene)){
+       transcript_by_gene <- marker_mtx[marker_mtx$gene == g,]$transcript
+       
+       # filter marker to only IDs present in obj:
+       status <- unique(transcript_by_gene) %in% rownames(seurat_obj@assays$RNA@counts)
+       markers_filtered <- unique(transcript_by_gene)[status]
+       
+       if (length(markers_filtered > 0)) {
+         
+         plot_markers_by_list(seurat_obj,
+                              markers_filtered,
+                              paste0(out_dir_mtx, feature, '/'),
+                              paste0(g, '.'))
+         
+       } else {
+         
+         message(paste0("Transcripts from gene ", g, " were not found in the current object"))
+         
+       }
+    }
+  }
+}
